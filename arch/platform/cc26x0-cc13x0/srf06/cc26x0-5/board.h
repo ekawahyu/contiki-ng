4bf1e53a90d62c5aa9eb9d731216b275c81e9a80
Eka Susilo <ekawahyu@yahoo.com>
2025-02-07 20:26:30 -0800

    updated board string to distinguish CC26x0EM models

--- arch/platform/cc26x0-cc13x0/srf06/cc26x0-5/board.h
+++ arch/platform/cc26x0-cc13x0/srf06/cc26x0-5/board.h
@@ -240,7 +240,7 @@
  * \name Device string used on startup
  * @{
  */
-#define BOARD_STRING "TI SmartRF06EB + CC26xx EM"
+#define BOARD_STRING "TI SmartRF06EB + CC26x0EM-5XD"
 /** @} */
 /*---------------------------------------------------------------------------*/
 #endif /* BOARD_H_ */
